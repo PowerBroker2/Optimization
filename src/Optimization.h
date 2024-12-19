1856f2ab5cb5bd8dae4416fa79ef5a0327259b8c
PowerBroker2 <gitstuff2@gmail.com>
2024-12-19 15:36:38 -0500

    Comment out things

--- src/Optimization.h
+++ src/Optimization.h
@@ -12,40 +12,40 @@
 using namespace Eigen;
 
 
-void printVecXd(const VectorXd& vec,
-                const int&      p=5,
-                Stream&         stream=Serial)
-{
-    for (int i=0; i<vec.rows(); i++)
-    {
-        if (vec(i) >= 0)
-            Serial.print(' ');
+// void printVecXd(const VectorXd& vec,
+//                 const int&      p=5,
+//                 Stream&         stream=Serial)
+// {
+//     for (int i=0; i<vec.rows(); i++)
+//     {
+//         if (vec(i) >= 0)
+//             Serial.print(' ');
         
-        stream.println(vec(i), p);
-    }
-}
+//         stream.println(vec(i), p);
+//     }
+// }
 
 
-void printMatXd(const MatrixXd& mat,
-                const int&      p=5,
-                Stream&         stream=Serial)
-{
-    for (int i=0; i<mat.rows(); i++)
-    {
-        for (int j=0; j<mat.cols(); j++)
-        {
-            if (mat(i, j) >= 0)
-                Serial.print(' ');
+// void printMatXd(const MatrixXd& mat,
+//                 const int&      p=5,
+//                 Stream&         stream=Serial)
+// {
+//     for (int i=0; i<mat.rows(); i++)
+//     {
+//         for (int j=0; j<mat.cols(); j++)
+//         {
+//             if (mat(i, j) >= 0)
+//                 Serial.print(' ');
         
-            stream.print(mat(i, j), p);
+//             stream.print(mat(i, j), p);
 
-            if (j != (mat.cols() - 1))
-                stream.print(", ");
-        }
+//             if (j != (mat.cols() - 1))
+//                 stream.print(", ");
+//         }
 
-        stream.println();
-    }
-}
+//         stream.println();
+//     }
+// }
 
 
 
