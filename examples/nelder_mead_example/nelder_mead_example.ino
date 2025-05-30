98190af3e10ece006a9d58480ea08a42d1734cd9
PowerBroker2 <gitstuff2@gmail.com>
2025-05-30 00:11:31 -0400

    Begin to fill out example

--- examples/nelder_mead_example/nelder_mead_example.ino
+++ examples/nelder_mead_example/nelder_mead_example.ino
@@ -3,27 +3,50 @@
 
 double function(const VectorXd& x)
 {
-  return 0;
+    VectorXd x_real(3);
+    x_real << 0, 1, 2;
+
+    VectorXd cross_product(3);
+    cross_product << ( (x(1) * x_real(2)) - (x(2) * x_real(1))),
+                     (-(x(0) * x_real(2)) + (x(2) * x_real(0))),
+                     ( (x(0) * x_real(1)) - (x(1) * x_real(0)));
+    
+    Serial.println();
+    printVecXd(cross_product);
+    Serial.println();
+
+    return cross_product.norm();
 }
 
 void setup()
 {
-  Serial.begin(115200);
-  
-  VectorXd x_start(4);
-  x_start << 2, 1, 3, 4;
-  
-  MatrixXd mat(3, 4);
-  mat << 2, 1, 3, 4,
-         5, 6, 7, 8,
-         4, 3, 2, 1;
-  
-  printMatXd(init_simplex_args(x_start));
+    Serial.begin(115200);
 
-  auto res = Nelder_Mead_Optimizer(function, x_start, 0.1, 10e-10);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    
+    VectorXd x_start(3);
+    x_start << 0, 1, 2;
+    
+    MatrixXd mat(3, 3);
+    mat << 1, 0, 0,
+           0, 1, 0,
+           0, 0, 1;
+    
+    Serial.println("x_start");
+    printVecXd(x_start);
+    Serial.println();
+    Serial.println("init simplex args");
+    printMatXd(init_simplex_args(x_start));
+    Serial.println();
+    Serial.println("init simplex results");
+    printVecXd(get_simplex_results(function,
+                                   init_simplex_args(x_start)));
+
+    //auto res = Nelder_Mead_Optimizer(function, x_start, 0.1, 10e-10);
 }
 
 void loop()
 {
-  
+    
 }
