7fbb9cd3f10a349c2d9a33f5dae2134c77c09f01
PB2 <gitstuff2@gmail.com>
2025-07-02 16:38:03 -0400

    Clean up example sketch

--- examples/nelder_mead_example/nelder_mead_example.ino
+++ examples/nelder_mead_example/nelder_mead_example.ino
@@ -1,53 +1,5 @@
 #include "Optimization.h"
-
-
-void printVecXd(const VectorXd& vec,
-                const int&      p=5,
-                Stream&         stream=Serial)
-{
-    for (int i=0; i<vec.rows(); i++)
-    {
-        if (vec(i) >= 0)
-            Serial.print(' ');
-        
-        stream.println(vec(i), p);
-    }
-}
-
-
-void printVecXi(const VectorXi& vec,
-                Stream&         stream=Serial)
-{
-    for (int i=0; i<vec.rows(); i++)
-    {
-        if (vec(i) >= 0)
-            Serial.print(' ');
-        
-        stream.println(vec(i));
-    }
-}
-
-
-void printMatXd(const MatrixXd& mat,
-                const int&      p=5,
-                Stream&         stream=Serial)
-{
-    for (int i=0; i<mat.rows(); i++)
-    {
-        for (int j=0; j<mat.cols(); j++)
-        {
-            if (mat(i, j) >= 0)
-                Serial.print(' ');
-        
-            stream.print(mat(i, j), p);
-
-            if (j != (mat.cols() - 1))
-                stream.print(", ");
-        }
-
-        stream.println();
-    }
-}
+#include "printing_helpers.h"
 
 
 double function(const VectorXd& x)
